ba0a9d6401c44d5849c5bbcb06bc1117bb4c826d
terulet <borras.eloi@gmail.com>
2026-07-07 16:03:32 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // La Gelateria de Roses · Tarjeta v4 premium
-const CACHE = 'gelateria-v2-15-final-no-special-prize';
+const CACHE = 'gelateria-v2-16-staff-kiosco';
 const SHELL = ['./', './index.html', './staff.html', './privacidad.html', './manifest.json', './manifest-staff.json', './firebase-config.js', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
